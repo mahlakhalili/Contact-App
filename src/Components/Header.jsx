356567858e038716d0ca1aadd89ebcc2e1a92111
Mahla <Mahlakhalili1@gmail.com>
2024-02-01 11:07:59 -0800

    up

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import styles from "./Header.module.css"
 
 function Header(){
     return(
-        <div>
+        <div className="{styles.container}">
             <h1>Contact App</h1>
+            <p>
+                <a href="#">Contact Link</a> | React JS Project
+            </p>
         </div>
     )
 }
