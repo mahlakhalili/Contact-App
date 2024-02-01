494c10709a8998bf1aff6a0cd8f3c539ddef7639
Mahla <Mahlakhalili1@gmail.com>
2024-02-01 10:44:03 -0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Header from './Components/Header.jsx'
 
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>hello react</h1>
+      <Header />
     </>
   )
 }
