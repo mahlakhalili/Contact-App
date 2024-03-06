486a003cd5d401c7319df550ae28f9814738fd28
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-03-06 08:38:52 -0800

    Update ContactsList.jsx

--- src/Components/ContactsList.jsx
+++ src/Components/ContactsList.jsx
@@ -4,11 +4,25 @@ function ContactsList({contacts}){
     return(
         <div>
             <h3>Contact List</h3>
-            <ul>
-                {
-                    contacts.map((contact) => (<li key={contact.id}>{contact.name}</li>))
-                }
-            </ul>
+            {
+                contacts.length ? (<ul>
+                    {
+                        contacts.map((contact) => (<li key={contact.id}>
+                            <p>{contact.name} {contact.lastName}</p>
+                            <p>
+                                {contact.email}
+                            </p>
+                            <p>
+                                {contact.phone}
+                            </p>
+                            <button></button>
+                        </li>))
+                    }
+                </ul>) 
+                : (<p>No Contacts Yet!</p>)
+
+            }
+            
         </div>
     )
 }
