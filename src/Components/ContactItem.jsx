07dd472977305d2cc30ab25e8135b4eae437ff2c
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-03-10 00:42:54 -0800

    Update ContactItem.jsx

--- src/Components/ContactItem.jsx
+++ src/Components/ContactItem.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
+import styles from "./ContactItem.module.css"
+
 function ContactItem({data : {name, id, lastName, phone, email} , deleteHandler}){
     return(
-        <li key={id}>
+        <li  className={styles.item}>
                 <p>{name} {lastName}</p>
                 <p>
                     {email}
